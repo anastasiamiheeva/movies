1613395d4c15f4ef85a3b937bc66f5d6b4bb1392
Anastasia Miheeva <mikheevaanastasiia93@yandex.ru>
2021-09-14 15:34:04 +0300

    pagination added

--- src/App.js
+++ src/App.js
@@ -19,9 +19,41 @@ function App() {
   const [page, setPage] = useState(1)
   const sortedAndSearchedMovies = useMovies(movies, filter.sort, filter.query)
   let pagesArray = getPagesArray(totalPages)
+  console.log(pagesArray)
 
+  console.log(totalPages)
+   //pagination
+  const [numberLimit, setPageNumberLimit] = useState(6)
+  const [maxPageNumberLimit, setMaxPageNumberLimit] = useState(7)
+  const [minPageNumberLimit, setMinPageNumberLimit] = useState(1)
+
+  const handleNextBtn = () => {
+    setPage(page + 1)
+    if(page + 1 >= maxPageNumberLimit) {
+      setMaxPageNumberLimit(maxPageNumberLimit + numberLimit)
+      setMinPageNumberLimit(minPageNumberLimit + numberLimit)
+    }
+    
+  }
+  const handlePrevBtn = () => {
+    setPage(page - 1)
+    if(page - 1 < minPageNumberLimit) {
+      setMinPageNumberLimit(minPageNumberLimit - numberLimit)
+      setMaxPageNumberLimit(maxPageNumberLimit - numberLimit)
+    }
+  }
+
+  let pageIncrementBtn = null;
+  if(totalPages > maxPageNumberLimit) {
+    pageIncrementBtn = <li className="page" onClick={handleNextBtn}>&hellip;</li>
+  }
   
- 
+  let pageDecrementBtn = null;
+  if(minPageNumberLimit > 1) {
+    pageDecrementBtn = <li className="page" onClick={handlePrevBtn}>&hellip;</li>
+  }
+
+
   const [fetchMovies, isMoviesLoading, movieError ] = useFetching(async () => {
     const response = await MovieService.getAll(limit, page)
     setMovies(response.movies)
@@ -29,17 +61,16 @@ function App() {
     setTotalPages(getPageCount(totalCount,limit))
   })
 
-  console.log(totalPages);
   
   useEffect(() => {
+    console.log(page)
     // fetchMovies(limit, page);
     fetchMovies()
   }, [page])
 
   const changePage = (page) => {
     setPage(page)
-    // fetchMovies(limit, page);
-    
+    // fetchMovies(limit, page); 
   }
  
   const removeMovie = (movie) => {
@@ -49,29 +80,52 @@ function App() {
   return (
     <div className="App">
       <Header />
+
+      {/*//////////////////// */}
+      <ul className="page__wrapper">
+        <li>
+            <button 
+            disabled={page === pagesArray[0] ? true : false}
+            onClick={handlePrevBtn}
+            className="page__button"
+          >
+            Prev
+          </button>
+        </li> 
+        {pageDecrementBtn}
+        {pagesArray.map( p => 
+          p < maxPageNumberLimit && p >= minPageNumberLimit
+          ?  <li 
+                className={page === p ? 'page page__current' : 'page'}
+                key={p}
+                onClick={() => changePage(p)}
+              >
+                {p}
+              </li>
+          : null
+        )}
+        {pageIncrementBtn}
+        <li>
+          <button 
+              disabled={page === pagesArray[pagesArray.length - 1] ? true : false}
+              className="page__button"
+              onClick={handleNextBtn}
+          >Next</button>
+        </li>
+      </ul>
+      {/*/////////////////// */}
+
       <MovieFilter filter={filter} setFilter={setFilter} />
-      {/* {movieError && 
+      {movieError && 
         <h1 style={{textAlign:'center', color: 'red'}}>Error: {movieError}</h1>
-      } */}
+      }
       {isMoviesLoading 
         ? <Loader/>
         : <MovieList 
         remove={removeMovie} 
         movies={sortedAndSearchedMovies}
         />
-      
       }
-      <ul className="page__wrapper">
-        {pagesArray.map( p => 
-          <li 
-            className={page === p ? 'page page__current' : 'page'}
-            key={p}
-            onClick={() => changePage(p)}
-          >
-            {p}
-          </li>
-        )}
-      </ul>
     </div>
   );
 }
